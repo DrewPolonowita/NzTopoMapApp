268d5dc28a86b14e4a0c4400d72e3471a1912922
AbrasiveAlmond <qpearson.nz@gmail.com>
2026-07-08 20:00:21 +1200

    todo list

--- modules/map/components/MapViewComponent.tsx
+++ modules/map/components/MapViewComponent.tsx
@@ -1,12 +1,15 @@
 import { View } from "react-native";
-import { Map, StyleSpecification, Camera } from "@maplibre/maplibre-react-native";
+import { useState, useEffect } from "react";
+import {
+  Map,
+  StyleSpecification,
+  Camera,
+} from "@maplibre/maplibre-react-native";
 
 import topoStyle from "../../../assets/json/nz-topo-style-spec.json";
 import cameraStyle from "../../../assets/json/nz-camera-style-spec.json";
 
-
 export default function MapViewComponent() {
-
   return (
     <View
       style={{
@@ -16,11 +19,11 @@ export default function MapViewComponent() {
     >
       <Map mapStyle={topoStyle as StyleSpecification}>
         <Camera
-            initialViewState={{
-                    center: [172.6207487, -43.52872608],
-                    zoom: 8
-                }}
-         />
+          initialViewState={{
+            center: [172.6207487, -43.52872608],
+            zoom: 8,
+          }}
+        />
       </Map>
     </View>
   );
@@ -32,4 +35,16 @@ Work out how to set maplibre to use imageSource
 work out how to serve locally stored images to that source.
 Look at the format that downloaded LINZ data comes in (either GeoTIFF or GeoJPEG).
 work ot how to link XYZ api to serve imageSource georeferenced images
+*/
+
+/*
+OfflineManager.createPack({
+  mapStyle: topoStyle,
+  minZoom: 0,
+  maxZoom: 15,
+  bounds: [
+    165, -48,
+    180, -33
+  ]
+});
 */
